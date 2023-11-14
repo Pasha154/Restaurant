d1b18c2fcc25e35343514e305fe99542b25813dd
Pasha154 <pavel154k@gmail.com>
2023-11-14 23:55:50 +0200

    Restaurant

---
+++ js.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector('#burger-menu')
+const menu = document.querySelector('#menu')
+
+burger.addEventListener('click',() =>{
+    menu.classList.toggle('disp');
+
+});
